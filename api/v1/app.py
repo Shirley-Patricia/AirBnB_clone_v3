63ab672910fe4c6cf4fbbde59224ffacedd9cb85
Shirley <shircar16@gmail.com>
2022-02-05 16:08:28 -0500

    include python interpreter and branch in get method in index.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 '''Entry point for the app'''
 from api.v1.views import app_views
 from flask import Flask
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 '''create a route /status on the object app_views that returns a JSON
 with status code'''
 from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods='GET', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status_json():
-    ''''''
+    '''return status'''
     return jsonify({"status": "OK"})
