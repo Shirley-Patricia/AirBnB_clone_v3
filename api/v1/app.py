4b2dcbc0571d9e72891d0a5de3523205eaee2d3c
eylens <eymasac@gmail.com>
2022-02-04 21:50:42 -0500

    change the way to get getenv

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,6 +8,9 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+host = getenv('HBNB_API_HOST') or '0.0.0.0'
+port = getenv('HBNB_API_PORT') or 5000
+
 
 @app.teardown_appcontext
 def teardown_appcontext(self):
@@ -16,6 +19,6 @@ def teardown_appcontext(self):
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(getenv('HBNB_API_PORT', '5000')),
+    app.run(host=host,
+            port=port,
             threaded=True)
