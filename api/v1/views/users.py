db0f71e975e943054bd25c17117c3bbb65cd5a55
Shirley <shircar16@gmail.com>
2022-02-07 11:02:12 -0500

    the way to raise error with abort was wrong, the code of createUser was modified

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -21,7 +21,7 @@ def listUser():
                  strict_slashes=False)
 def objUser(user_id):
     '''retrieves a User object according to its user_id'''
-    obj = storage.get(User, user_id)
+    obj = storage.get('User', user_id)
     if obj:
         return jsonify(obj.to_dict())
     else:
@@ -31,7 +31,7 @@ def objUser(user_id):
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def deleteUser(user_id):
     '''Deletes a User object according to its user_id'''
-    obj = storage.get(User, user_id)
+    obj = storage.get('User', user_id)
     if obj:
         storage.delete(obj)
         storage.save()
@@ -44,32 +44,32 @@ def deleteUser(user_id):
 def createUser():
     '''Creates a User'''
     data_request = request.get_json()
-    if not data_request:
-        return abort(400, 'Not a JSON')
-    if 'email' not in data_request:
-        return abort(400, 'Missing email')
-    if 'password' not in data_request:
-        return abort(400, 'Missing password')
-    else:
+    if type(data_request) is dict:
+        if 'email' not in data_request.keys():
+            abort(400, 'Missing email')
+        if 'password' not in data_request.keys():
+            abort(400, 'Missing password')
         obj = User(**data_request)
         storage.new(obj)
         storage.save()
         return jsonify(obj.to_dict()), 201
+    else:
+        abort(400, 'Not a JSON')
 
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def updateUser(user_id):
     '''Updates a User object according to its user_id'''
-    obj = storage.get(User, user_id)
+    obj = storage.get('User', user_id)
     if obj:
         data_request = request.get_json()
-        if data_request:
+        if type(data_request) is dict:
             noKeys = ['id', 'email', 'created_at', 'updated_at']
             for key, value in data_request.items():
                 if key not in noKeys:
                     setattr(obj, key, value)
-                storage.save()
-                return jsonify(obj.to_dict()), 200
+            storage.save()
+            return jsonify(obj.to_dict()), 200
         else:
             abort(400, 'Not a JSON')
     else:
